d6a1f9496892edacc68b40af6e7e4e70d096e104
Basit Nawaz <basitnawaz507@gmail.com>
2022-06-21 12:19:05 +0500

    Full source code for react beautiful dnd

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ItemsList from "./Components/ItemsList";
+
+function App() {
+	return (
+		<div className="App">
+			<ItemsList />
+		</div>
+	);
+}
+
+export default App;
